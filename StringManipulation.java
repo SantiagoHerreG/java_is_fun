5fe30044750076f82eddefdb282cbc08b2d77968
SantiagoHerreG <888@holbertonschool.com>
2019-11-30 01:56:10 +0000

    Substrings

---
+++ StringManipulation.java
@@ -0,0 +1,8 @@
+public class StringManipulation
+{
+	public static void main(String[] args)
+	{
+		String hello = "Hello world";
+		System.out.printf("%s%s%s%s%n", hello, hello, hello, hello.substring(0, 2));
+	}
+}
