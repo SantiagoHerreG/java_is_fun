0c90b4cdf9da01a7a85c6c8d2d0382a7abd0340d
SantiagoHerreG <888@holbertonschool.com>
2020-04-09 00:06:57 +0000

    Head first Java, book for learning java by doing exercises

---
+++ head_first_java/c1_Basic/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+
+  public static void main (String[] args) {
+
+    System.out.println("I Rule");
+    System.out.println("My home");
+
+    int x = 0;
+
+    for (; x < 10; x++) {
+      System.out.println(x);
+    }
+
+  }
+
+}
